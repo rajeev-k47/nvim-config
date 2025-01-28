78b9aff6412f80ebdbf15e62fee4fff4cca0353a
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-28 20:30:33 +0530

    <Typo:/*>

--- lua/plugins/cp.lua
+++ lua/plugins/cp.lua
@@ -9,8 +9,8 @@ return {
       vim.keymap.set("n", "<leader>cpa", ":CompetiTest add_testcase<CR>", { desc = "add_testcase" })
       vim.keymap.set("n", "<leader>cpe", ":CompetiTest edit_testcase<CR>", { desc = "edit_testcase" })
       vim.keymap.set("n", "<leader>cps", ":CompetiTest show_ui<CR>", { desc = "show_ui" })
-      vim.keymap.set("n", "<leader>cpp", ":CompetiTest recieve problem<CR>", { desc = "Fetch a problem" })
-      vim.keymap.set("n", "<leader>cpc", ":CompetiTest recieve contest<CR>", { desc = "Fetch contest" })
+      vim.keymap.set("n", "<leader>cpp", ":CompetiTest receive problem<CR>", { desc = "Fetch a problem" })
+      vim.keymap.set("n", "<leader>cpc", ":CompetiTest receive contest<CR>", { desc = "Fetch contest" })
     end,
   },
 }
